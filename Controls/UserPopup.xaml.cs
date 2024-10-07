c025356c5de216e4fc58826082851726f36eccae
Artur <am3044288gmail.com>
2024-10-07 20:54:16 +0700

    Basic GUI

---
+++ Controls/UserPopup.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Windows.Controls;
+
+
+namespace SchoolAutoShedule.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для UserPopup.xaml
+    /// </summary>
+    public partial class UserPopup : UserControl
+    {
+        public UserPopup()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,8 @@
-﻿using System.Text;
+﻿
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace SchoolAutoShedule
 {
@@ -20,5 +15,92 @@ namespace SchoolAutoShedule
         {
             InitializeComponent();
         }
+        private void hidePopup(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+        private void ShowPopup(UIElement element, string text)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = element;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                Header.PopupText.Text = text;
+            }
+        }
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
+        }
+
+        // Start: MenuLeft PopupButton //
+        private void btnTeachers_MouseEnter(object sender, MouseEventArgs e)        => ShowPopup(btnTeachers, "Учителя");
+        private void btnSchProgram_MouseEnter(object sender, MouseEventArgs e)      => ShowPopup(btnSchProgram, "Шк. Программа");
+        private void btnSubjects_MouseEnter(object sender, MouseEventArgs e)        => ShowPopup(btnSubjects, "Предметы");
+        private void btnSchInfo_MouseEnter(object sender, MouseEventArgs e)         => ShowPopup(btnSchInfo, "Инфа О Школе");
+        private void btnCreateSchedule_MouseEnter(object sender, MouseEventArgs e)  => ShowPopup(btnCreateSchedule, "Создать Расписание");
+        private void btnExportSchedule_MouseEnter(object sender, MouseEventArgs e)  => ShowPopup(btnExportSchedule, "Экспортировать Расписание");
+        private void btnSetting_MouseEnter(object sender, MouseEventArgs e)         => ShowPopup(btnSetting, "Настройки");
+        // End: MenuLeft PopupButton //
+
+        // Start: Button Close | Restore | Minimize 
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        // End: Button Close | Restore | Minimize
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            fContainer.Navigate(new System.Uri("Pages/Teachers.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void btnSubjects_Click(object sender, RoutedEventArgs e)
+        {
+            fContainer.Navigate(new System.Uri("Pages/Subjects.xaml", UriKind.RelativeOrAbsolute));
+        }
+        private void btnSchProgram_Click(object sender, RoutedEventArgs e)
+        {
+            fContainer.Navigate(new System.Uri("Pages/SchProgram.xaml", UriKind.RelativeOrAbsolute));
+        }
+        private void btnSchInfo_Click(object sender, RoutedEventArgs e)
+        {
+            fContainer.Navigate(new System.Uri("Pages/SchInfo.xaml", UriKind.RelativeOrAbsolute));
+        }
+        private void btnScheduleCreator_Click(object sender, RoutedEventArgs e)
+        {
+            fContainer.Navigate(new System.Uri("Pages/ScheduleCreator.xaml", UriKind.RelativeOrAbsolute));
+        }
+        private void btnExportSchedule_Click(object sender, RoutedEventArgs e)
+        {
+            fContainer.Navigate(new System.Uri("Pages/Export.xaml", UriKind.RelativeOrAbsolute));
+        }
+        private void btnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            fContainer.Navigate(new System.Uri("Pages/Settings.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+
+        private void WindowMove(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+
     }
 }
